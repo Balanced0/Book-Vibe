bed6601a53405508c44ac3b659a218146784c209
balanced0 <alvichowdhury013@gmail.com>
2026-04-09 18:40:57 +0600

    feat: implement responsive Navbar component and set up routing with Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import MainLayout from './layout/MainLayout';
-import Homepage from './pages/homepage/Homepage';
-import Books from './pages/books/Books';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-    children: [{
-      index: true,
-      element: <Homepage></Homepage>,
-    },
-    {
-      path: "/books",
-      element: <Books></Books>,
-    }
-  ]
-  },
-]);
+import { router } from "./routes/Routes"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
