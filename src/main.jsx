8b6f444ad5c89f14448940a678861033bf2386b2
balanced0 <alvichowdhury013@gmail.com>
2026-04-09 16:14:27 +0600

    feat: add routing with react-router, create main layout and pages for homepage and books

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import MainLayout from './layout/MainLayout';
+import Homepage from './pages/homepage/Homepage';
+import Books from './pages/books/Books';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [{
+      index: true,
+      element: <Homepage></Homepage>,
+    },
+    {
+      path: "/books",
+      element: <Books></Books>,
+    }
+  ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h3>Hello</h3>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
